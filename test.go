4f64a68fa07a4199a9ad076ae3dd9975e210ab34
Zhuguang <classnet@sina.com>
2016-10-01 15:14:13 +0800

    Create test.go

---
+++ test.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/classnet/redis"
+
+func main() {
+    var client redis.Client
+    var key = "hello"
+    client.Set(key, []byte("world"))
+    val, _ := client.Get("hello")
+    println(key, string(val))
+}
